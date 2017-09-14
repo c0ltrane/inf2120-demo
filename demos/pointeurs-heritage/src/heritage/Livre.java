8b934b545b80b0b50c30f2f27f790c048845e54c
Thomas <thom.rebaud@hotmail.com>
2017-09-14 12:44:17 -0400

    minor fix

---
+++ demos/pointeurs-heritage/src/heritage/Livre.java
@@ -0,0 +1,11 @@
+package heritage;
+
+/**
+ * Created by thomas on 9/11/17.
+ */
+public class Livre extends TaxeSimple {
+
+    public Livre(double prixEtalage) {
+        super(prixEtalage);
+    }
+}
